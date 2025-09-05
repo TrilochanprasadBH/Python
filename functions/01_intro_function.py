5c2cc3c033b6d8b75f939fa3f2abc987bb4cf687
TrilochanprasadBH <tptrilochanprasad@gmail.com>
2025-09-05 20:02:11 +0530

    split funcs

---
+++ functions/01_intro_function.py
@@ -0,0 +1,12 @@
+# def print_order (param1, param2):
+#     print(f"I would like to have {param1} and {param2}.")
+
+# print_order("arg1","arg2");
+
+
+def HandleChai(name, typeOfChai):
+    print(f"Hello {name}, would you like some {typeOfChai} chai?")
+
+
+HandleChai("Alice", "masala"); 
+
---
+++ functions/02_splitting_func.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("Fetching sales data...")
+
+
+def fetch_customers():
+    print("Fetching customer data...")
+   
+
+
+def create_report():
+    fetch_sales()
+    fetch_customers()
+    print("Creating report...")
+   
+
+create_report()
